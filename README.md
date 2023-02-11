14518d961650f8d968029e71b747e2d1c34b774a
dusdalemilie <111698379+dusdalemilie@users.noreply.github.com>
2023-02-11 08:06:43 -0700

    Appearance Change
    Change to python, html, and css files.

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was created as the capstone project for the Fullstack DevOps bootcamp.
+
+This quiz application uses HTML, CSS, and Javascript. This application gathers user information from quiz questions to assess whether the responses are correct, then uses the information to calculate a score on a final page of results.
--- app.py
+++ app.py
@@ -13,6 +13,20 @@ def quiz():
     quiz_data = run_quiz("questions.xlsx")
     return render_template('quiz.html', quiz=quiz_data)
 
+@quiz_blueprint.route("/total", methods=["GET", "POST"])
+def total():
+    quiz = run_quiz("questions.xlsx")
+    answers = []
+    correct_answer = []
+    for i in range(len(quiz)):
+        answer = request.form.get(f'{quiz[i]["question"]}')
+        answers.append(answer)
+        correct_answer = request.form.getlist(f'correct_option{i}')
+        correct_answer.append(correct_answer)
+    result = display_score(quiz, answers)
+    score = result[0]
+    incorrect_answers = result[1]
+    return render_template("total.html", score=score, incorrect_answers=incorrect_answers)
     
 @quiz_blueprint.route("/score", methods=["GET", "POST"])
 def score():
